14163f3095a451f2c3e9a9b544e9d29626348c97
olya-antre <antropoffa@gmail.com>
2024-04-02 21:46:53 +0300

    Update bot (digest), postgresql, readme

--- src/backend/models.py
+++ src/backend/models.py
@@ -1,25 +1,25 @@
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, DateTime, Text
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import declarative_base
 
 
 # родительский класс для наследования остальных
-class Base(DeclarativeBase):
-    created = Column(DateTime, default=datetime.now())
-    updated = Column(DateTime, default=datetime.now(), onupdate=datetime.now)
+Base = declarative_base()
 
 
 # класс для новостей, ключ из класса категории, чтобы завести отношение
 class News(Base):
     __tablename__ = "News"
 
-    id = Column(Integer(), primary_key=True, autoincrement=True)
-    title = Column(String(150))
-    url = Column(String(200), nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String(300))
+    url = Column(String(300), nullable=False)
     content = Column(Text())
     summary = Column(Text())
     category_id = Column(Integer(), nullable=False)
     score = Column(Integer())
+    created = Column(DateTime, default=datetime.now())
+    updated = Column(DateTime, default=datetime.now(), onupdate=datetime.now)
 
 
 # класс для категории (нужен для нормальной архитектуры бд)
@@ -28,15 +28,19 @@ class Categories(Base):
 
     id = Column(Integer(), primary_key=True, autoincrement=True)
     name = Column(String(100))
+    created = Column(DateTime, default=datetime.now())
+    updated = Column(DateTime, default=datetime.now(), onupdate=datetime.now)
 
 
 # класс для пользователей бота
 class Users(Base):
     __tablename__ = "Users"
 
-    id = Column(Integer(), primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     username = Column(String(100))
     chat = Column(String(100))
+    created = Column(DateTime, default=datetime.now())
+    updated = Column(DateTime, default=datetime.now(), onupdate=datetime.now)
 
 
 # класс для хранения выбранных категорий пользователями бота
@@ -44,5 +48,7 @@ class UserCategories(Base):
     __tablename__ = "User_categories"
 
     id = Column(Integer(), primary_key=True, autoincrement=True)
-    user_id = Column(Integer())
-    category_id = Column(String(100))
+    user_id = Column(String(100))
+    category_id = Column(Integer)
+    created = Column(DateTime, default=datetime.now())
+    updated = Column(DateTime, default=datetime.now(), onupdate=datetime.now)
--- src/tg_bot/bot_database/engine.py
+++ src/tg_bot/bot_database/engine.py
@@ -1,6 +1,9 @@
 import os
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
-engine = create_async_engine(os.getenv('DB_LITE_ASYNC'))
 
-session_maker = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+print(os.getenv('DB_POSTGRESQL_ASYNC'))
+engine = create_async_engine(os.getenv('DB_POSTGRESQL_ASYNC'))
+
+session_maker = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
